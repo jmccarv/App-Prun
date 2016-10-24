3b3a8ecfa617e65137243e61d49d7aa14208e3fa
Jason McCarver <slam@parasite.cc>
2016-10-24 10:00:25 -0500

    Fix no version dependency for Test::Script

--- lib/App/Prun.pm
+++ lib/App/Prun.pm
@@ -4,7 +4,7 @@ use Moo;
 use Storable qw( freeze );  # to support testing
 use namespace::clean;
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 has pm => ( is => 'ro', required => 1 );
 has report_failed_procs => ( is => 'ro', default => 1 );
@@ -91,7 +91,7 @@ App::Prun - Provides the prun script as a command line interface to L<Parallel::
 
 =head1 VERSION
 
-Version 1.02
+Version 1.03
 
 =head1 SYNOPSYS
 
